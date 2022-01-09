d5688de59737f9f86c97464c911a4d0069e40956
alex <probesalesman@gmail.com>
2022-01-09 16:15:37 -0500

    initial types to work with

---
+++ src/utils/types.ts
@@ -0,0 +1,23 @@
+export interface ImageData {
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  title: string;
+  url: string;
+  copyright?: string;
+}
+
+export interface LikeStatus {
+  liked: "LIKED" | "NOT_LIKED";
+}
+
+// opting for array instead of object for ease of key removal
+// as an array, adding or removing a key will be a linear operation, so if cache becomes large enough could become slow and might need to use object
+// for faster lookup (and deletion? Not positive about speed of removing a key from an object)
+export type LikesCache = string[];
+
+export interface LikeActions {
+  likeImage: (url: string) => void;
+  removeLike: (url: string) => void;
+}
