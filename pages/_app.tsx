181cd080128ef8fb9b1e589b74adb4928db5a11f
alex <probesalesman@gmail.com>
2022-01-10 17:08:07 -0500

    temp to be able to use fontawesome heart icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.12.1/css/all.css"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
